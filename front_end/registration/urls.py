6a075c46cd9661eaa35801c585e939297f8f965b
EimantasZilis <zilis.eimantas@gmail.com>
2020-04-12 09:21:57 +0100

    Customised login fields
    Removed labels and changed releant css

--- front_end/registration/urls.py
+++ front_end/registration/urls.py
@@ -6,7 +6,7 @@ from registration import views
 app_name = 'registration'
 
 urlpatterns = [
-    path('login/', auth_views.LoginView.as_view(), name='login'),
+    path('login/', views.SigninView.as_view(), name='login'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
     path('signup/', views.SignupView.as_view(), name='signup'),
     path('change_password/', auth_views.PasswordChangeView.as_view(), name='change_password'),
--- front_end/registration/views.py
+++ front_end/registration/views.py
@@ -1,12 +1,18 @@
+from django import forms
 from django.shortcuts import render
 from django.urls import reverse_lazy
 from django.views.generic import CreateView, TemplateView
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth.views import LoginView
 
-from registration import forms
-
+from registration import forms as registration_forms
 
 class SignupView(CreateView):
-    form_class = forms.UserCreateForm
+    form_class = registration_forms.UserCreateForm
     success_url = reverse_lazy('login')
     template_name = 'registration/signup.html'
+
+class SigninView(LoginView):
+    form_class = registration_forms.UserLoginForm
+    template_name = "registration/login.html"
 
