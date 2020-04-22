2815e16dbb59f8e48a83c2738a787e8040432d4c
EimantasZilis <zilis.eimantas@gmail.com>
2020-04-22 08:58:48 +0100

    Created signup completed view

--- front_end/registration/urls.py
+++ front_end/registration/urls.py
@@ -9,6 +9,7 @@ urlpatterns = [
     path('login/', views.SigninView.as_view(), name='login'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
     path('signup/', views.SignupView.as_view(), name='signup'),
+    path('signup_completed/', views.SignupCompletedView.as_view(), name='signup_completed'),
     path('change_password/', auth_views.PasswordChangeView.as_view(), name='change_password'),
     path('reset_password/', views.UserPasswordResetView.as_view(), name='reset_password'),
     path('password_reset/done/', auth_views.PasswordResetDoneView.as_view(), name='password_reset_done'),
--- front_end/registration/views.py
+++ front_end/registration/views.py
@@ -1,7 +1,7 @@
 from django import forms
 from django.shortcuts import render
 from django.urls import reverse_lazy
-from django.views.generic import CreateView, FormView
+from django.views.generic import CreateView, FormView, TemplateView
 from django.contrib.auth.forms import AuthenticationForm
 from django.contrib.auth.views import LoginView, PasswordResetView, PasswordResetConfirmView
 
@@ -9,12 +9,15 @@ from registration import forms as registration_forms
 
 class SignupView(CreateView):
     form_class = registration_forms.UserCreateForm
-    success_url = reverse_lazy('login')
+    success_url = reverse_lazy('registration:signup_completed')
     template_name = 'registration/signup.html'
 
 class SigninView(LoginView):
     form_class = registration_forms.UserLoginForm
     template_name = "registration/login.html"
+
+class SignupCompletedView(TemplateView):
+    template_name = "registration/signup_completed.html"
 
 class UserPasswordResetView(PasswordResetView):
     form_class = registration_forms.UserPasswordResetForm
