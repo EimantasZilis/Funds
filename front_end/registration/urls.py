0af154f886145ec267df6175fbdb132b6304f795
EimantasZilis <zilis.eimantas@gmail.com>
2020-05-08 19:30:42 +0100

    Implemented warnings using django messages
    Also removed some redundant templates and used messages to convey
    relevant information to the user

--- front_end/registration/urls.py
+++ front_end/registration/urls.py
@@ -9,10 +9,7 @@ urlpatterns = [
     path('login/', views.SigninView.as_view(), name='login'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
     path('signup/', views.SignupView.as_view(), name='signup'),
-    path('signup_completed/', views.SignupCompletedView.as_view(), name='signup_completed'),
     path('change_password/', auth_views.PasswordChangeView.as_view(), name='change_password'),
     path('reset_password/', views.UserPasswordResetView.as_view(), name='reset_password'),
-    path('password_reset/done/', auth_views.PasswordResetDoneView.as_view(), name='password_reset_done'),
     path('reset/<uidb64>/<token>/', views.UserPasswordResetConfirmView.as_view(), name='password_reset_confirm'),
-    path('password_reset/complete/', auth_views.PasswordResetCompleteView.as_view(), name='password_reset_complete')
 ]
