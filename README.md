d0793e28cecb447493b0b467f0da72854056b4b9
EimantasZilis <zilis.eimantas@gmail.com>
2020-08-12 20:15:43 +0100

    Set up install.sh file

--- README.md
+++ README.md
@@ -1,36 +1,10 @@
 # Spending Tree
 
 ## Installation instructions
-### Install conda
-Download the latest conda release from [here](https://www.anaconda.com/distribution/).
-
-### Configure environment
-Create a new environment and install install relevant packages by running:
-```conda env create -f build-mac.yml```
-
-### Install brew packages (MacOS)
+Download the latest conda release from [here](https://www.anaconda.com/distribution/) and run
 ```
-brew install sass/sass/sass
+bash install.sh
 ```
-
-### Install and configure postgres
-```
-brew install postgresql
-brew postgresql-upgrade-database
-brew services start postgres
-psql
-CREATE USER spuser WITH PASSWORD '1234';
-CREATE DATABASE spdb WITH OWNER 'postgres' TEMPLATE template0 ENCODING 'UTF8';   
-GRANT ALL PRIVILEGES ON DATABASE spdb to spuser;
-ALTER ROLE spuser SET timezone TO 'UTC';
-ALTER USER spuser CREATEDB;
-```
-
-### Compile CSS
-```sass front_end/static/base.scss front_end/static/base.css```
-
-### Git hooks
-```ln -s -f ../../hooks/pre-commit .git/hooks/pre-commit```
 
 ## To do
 ### High priority
--- install.sh
+++ install.sh
@@ -2,21 +2,25 @@
 
 echo "Installing dependencies..."
 conda env create -f build-mac.yml
+brew update
 brew install sass/sass/sass
 brew install postgresql
-brew postgresql-upgrade-database
 
 echo "Starting Postgres..."
 brew services start postgres
 
-echo "Setting up Postgres for the first time..."
-psql -U postgres -d spdb -c "SELECT * from registration_user"
-
-psql -U postgres -c "CREATE USER spuser WITH PASSWORD '1234';"
-psql -U postgres -c "CREATE DATABASE spdb WITH OWNER 'postgres' TEMPLATE template0 ENCODING 'UTF8';"
-psql -U postgres -c "GRANT ALL PRIVILEGES ON DATABASE spdb to spuser;"
-psql -U postgres -c "ALTER ROLE spuser SET timezone TO 'UTC';"
-psql -U postgres -c "ALTER USER spuser CREATEDB;"
+psql -U "$USER" -d postgres -c "CREATE USER spuser WITH PASSWORD '1234';"
+psql -U "$USER" -d postgres -c "CREATE DATABASE spdb WITH OWNER "$USER" TEMPLATE template0 ENCODING 'UTF8';"
+psql -U "$USER" -d postgres -c "GRANT ALL PRIVILEGES ON DATABASE spdb to spuser;"
+psql -U "$USER" -d postgres -c "ALTER ROLE spuser SET timezone TO 'UTC';"
+psql -U "$USER" -d postgres -c "ALTER USER spuser CREATEDB;"
 
 echo "Compiling CSS..."
 sass front_end/static/base.scss front_end/static/base.css
+
+echo "Applying Django migrations..."
+python front_end/manage.py migrate
+python front_end/manage.py makemigrations
+
+echo "Applying git hooks..."
+ln -s -f ../../hooks/pre-commit .git/hooks/pre-commit
